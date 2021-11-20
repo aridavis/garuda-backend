1cc77469c3192129eafe3d119280a846d60569fd
Ari Davis <ari.davis@gdn-commerce.com>
2021-11-20 13:25:53 +0700

    change migration naem

--- database/migrations/2010_11_20_052741_create_steps_table.php
+++ database/migrations/2010_11_20_052741_create_steps_table.php
@@ -15,6 +15,7 @@ class CreateStepsTable extends Migration
     {
         Schema::create('steps', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
